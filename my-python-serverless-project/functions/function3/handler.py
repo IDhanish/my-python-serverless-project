5f040a4d04307a1be01300c6d96e9a2bac5931bf
IDhanish <87757749+IDhanish@users.noreply.github.com>
2023-11-07 15:42:23 +0530

    Update handler.py

--- my-python-serverless-project/functions/function3/handler.py
+++ my-python-serverless-project/functions/function3/handler.py
@@ -1,10 +1,15 @@
 import json
 
 def handler(event, context):
-    # Your function logic here
-    response = {
-        "statusCode": 200,
-        "body": json.dumps("Hello from Function 3")
-    }
-
-    return response
+    if event["httpMethod"] == "GET":
+        return {
+            "statusCode": 200,
+            "body": json.dumps("Hello from Function 3 (GET)")
+        }
+    elif event["httpMethod"] == "POST":
+        # Your POST logic here
+        response = {
+            "statusCode": 200,
+            "body": json.dumps("Hello from Function 3 (POST)")
+        }
+        return response
